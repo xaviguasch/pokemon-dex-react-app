18f991de34e80454068c7006c8b7af1fd0a662b0
xaviguasch <xaviguasch@gmail.com>
2019-04-29 20:18:25 +0200

    added Pokegame compoment and isWinner logic

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -13,7 +13,7 @@ class Pokecard extends Component {
         <h1 className='Pokecard-title'>{this.props.name}</h1>
         <img src={imgSrc} alt={this.props.name} />
         <div className='Pokecard-data'>Type: {this.props.type}</div>
-        <div className='Pokecard-data'>Type: {this.props.exp}</div>
+        <div className='Pokecard-data'>EXP: {this.props.exp}</div>
       </div>
     )
   }
